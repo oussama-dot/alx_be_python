9237bd5aa97cc49aff3ee72298ba04896fa93471
oussama-dot <eloussamafouah@gmail.com>
2025-06-17 12:49:21 +0100

    arthemic

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,9 @@
+def perform_operation(num1,num2,one,two,three):
+    num1 = int(input("enter number one"))
+    num2 = int(input("enter number two"))
+    operation = input("enter which opertaion you want to ")
+    one = num2 + num1
+    two = num1 * num2
+    three = num1 / num2
+
+perform_operation()
