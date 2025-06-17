9a28d1db3a170f0869f790dc7147aacd47a00c2d
oussama-dot <eloussamafouah@gmail.com>
2025-06-17 12:52:03 +0100

    new project

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,9 +1,13 @@
-def perform_operation(num1,num2,one,two,three):
-    num1 = int(input("enter number one"))
-    num2 = int(input("enter number two"))
-    operation = input("enter which opertaion you want to ")
-    one = num2 + num1
-    two = num1 * num2
-    three = num1 / num2
-
-perform_operation()
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
+    else:
+        return "Error: Invalid operation"
