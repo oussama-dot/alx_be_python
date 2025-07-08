f64afe39cee79c4cd047b3a65f13a05437fca9f3
oussama-dot <eloussamafouah@gmail.com>
2025-07-08 10:47:03 +0100

    new programm

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,58 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False  # private attribute to track availability
+
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        return not self._is_checked_out
+
+
+class Library:
+    def __init__(self):
+        self._books = []  # private list of Book instances
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                if book.check_out():
+                    return True
+                else:
+                    # Book already checked out
+                    return False
+        # Book not found
+        return False
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                if book.return_book():
+                    return True
+                else:
+                    # Book wasn't checked out
+                    return False
+        # Book not found
+        return False
+
+    def list_available_books(self):
+        available_books = [book for book in self._books if book.is_available()]
+        if not available_books:
+            print("No books available.")
+            return
+        for book in available_books:
+            print(f"{book.title} by {book.author}")
